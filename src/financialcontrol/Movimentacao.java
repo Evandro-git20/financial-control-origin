385cc58d0ca05268e68b95b98083609b91c67ee7
evandro <evandro.ribeiro@unigranrio.br>
2021-08-30 17:55:34 -0300

    criando classe principal e movimentação

---
+++ src/financialcontrol/Movimentacao.java
@@ -0,0 +1,10 @@
+package financialcontrol;
+
+import java.util.Date;
+
+public class Movimentacao {
+	private String tipoDeMovimentacao;
+	private Date dataDaMovimentacao;
+	private Double valorDaMovimentacao;
+	private String observacaoDaMovimentacao;
+}
---
+++ src/financialcontrol/Principal.java
@@ -0,0 +1,19 @@
+package financialcontrol;
+
+import java.util.Scanner;
+
+public class Principal {
+//	Menu de ações
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); 
+		int opcao;
+		System.out.println("[1] Adicionar Movimentação Financeira");
+		System.out.println("[2] Alterar Movimentação Financeira");
+		System.out.println("[3] Remover Movimentação Financeira");
+		System.out.println("[4] Listar Movimentação Financeira");
+		System.out.println("[0] Sair");
+		opcao = in.nextInt();
+		
+	}
+
+}
