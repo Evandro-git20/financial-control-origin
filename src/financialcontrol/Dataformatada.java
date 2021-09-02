dfffec9b3727a63a7af87359a957ba6af706ac18
evandro <evandro.ribeiro@unigranrio.br>
2021-09-01 21:59:26 -0300

    Lógica de alterar criada

--- src/financialcontrol/Dataformatada.java
+++ src/financialcontrol/Dataformatada.java
@@ -3,6 +3,7 @@ package financialcontrol;
 import java.util.Date;
 
 public class Dataformatada {
+	@SuppressWarnings("unused")
 	private Date data;
 
 	public Dataformatada(Date data) {
--- src/financialcontrol/Principal.java
+++ src/financialcontrol/Principal.java
@@ -7,36 +7,55 @@ import java.util.Scanner;
 
 public class Principal {
 //	Menu de ações
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int opcao=2147483647;
 		
 
 		List <Movimentacao> movimento = new ArrayList<>();
 		Movimentacao movimentacao = new Movimentacao();
 		Date data = new Date();
-
-		System.out.println("\n[1] Adicionar Movimentação Financeira");
-		System.out.println("[2] Alterar Movimentação Financeira");
-		System.out.println("[3] Remover Movimentação Financeira");
-		System.out.println("[4] Listar Movimentação Financeira");
-		System.out.println("[0] Sair");
-		opcao = in.nextInt();
-
-		if(opcao == 1)
-		    System.out.print("Valor da movimentação: ");
-		    movimentacao.setValorDaMovimentacao(in.nextDouble());
-		    System.out.print("Observação: ");
-		    movimentacao.setObservacaoDaMovimentacao(in.next());
-		    movimentacao.setTipoDeMovimentacao("Entrada");
-		    movimentacao.setDataDaMovimentacao(data);
-		    movimento.add(movimentacao);
-
-			for (Movimentacao element : movimento) {
-				System.out.printf("\nTipo: "+element.getTipoDeMovimentacao()+
-						"    Observação: "+element.getObservacaoDaMovimentacao()+
-						"    Valor: "+element.getValorDaMovimentacao()+
-						"    Data: "+element.getDataDaMovimentacao());
-		}
+		
+		int opcao=2147483647;
+		do {
+			System.out.println("\n[1] Adicionar Movimentação Financeira");
+			System.out.println("[2] Alterar Movimentação Financeira");
+			System.out.println("[3] Remover Movimentação Financeira");
+			System.out.println("[4] Listar Movimentação Financeira");
+			System.out.println("[0] Sair");
+			opcao = in.nextInt();
+	
+			if(opcao == 1) {
+			    System.out.print("Valor da movimentação: ");
+			    movimentacao.setValorDaMovimentacao(in.nextDouble());
+			    System.out.print("Observação: ");
+			    movimentacao.setObservacaoDaMovimentacao(in.next());
+			    movimentacao.setTipoDeMovimentacao("Entrada");
+			    movimentacao.setDataDaMovimentacao(data);
+			    movimento.add(movimentacao);
+	
+				
+				System.out.print("\nTipo: "+movimentacao.getTipoDeMovimentacao()+
+						"    Observação: "+movimentacao.getObservacaoDaMovimentacao()+
+						"    Valor: "+movimentacao.getValorDaMovimentacao()+
+						"    Data: "+movimentacao.getDataDaMovimentacao());
+					
+			
+				System.out.print("\n\nNova movimentação: ");
+			    movimentacao.setValorDaMovimentacao(in.nextDouble());
+			    System.out.print("Observação: ");
+			    movimentacao.setObservacaoDaMovimentacao(in.next());
+			    movimentacao.setTipoDeMovimentacao("Entrada");
+			    movimentacao.setDataDaMovimentacao(data);
+			    movimento.add(movimentacao);
+	
+				
+				System.out.print("\nTipo: "+movimentacao.getTipoDeMovimentacao()+
+						"    Observação: "+movimentacao.getObservacaoDaMovimentacao()+
+						"    Valor: "+movimentacao.getValorDaMovimentacao()+
+						"    Data: "+movimentacao.getDataDaMovimentacao());
+			}
+			
+		}while(opcao>1);
 	}
 }
